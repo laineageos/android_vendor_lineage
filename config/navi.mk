8ad41c7f66a8fdc0c1670cad1f4bd697efe97eaf
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 07:58:33 +0800

    navi: Disable memtag for different processes
    * it defaults to async if not specified
    
    Change-Id: Ic3f992b8efc5b96f7055ec4a7e5fc3f7cc28be9a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/navi.mk
+++ config/navi.mk
@@ -33,9 +33,12 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 
-# Disable async MTE on system_server
+# Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    arm64.memtag.process.system_server=off
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
 
 # Enable dex2oat64 to do dexopt
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
