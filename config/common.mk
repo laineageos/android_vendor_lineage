adcc5207c8464fd069b85dda3c44ce4fbe072bbb
Pranav Vashi <neobuddy89@gmail.com>
2026-08-16 09:24:57 +0000

    Move face unlock biometric permission to system_ext
    Change-Id: I16214c9dc0fb4eba67a92a250731fd24841ff97b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.face.sense_service=true
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
 # Credential storage
